fe64175c89aa918130c7befe17b6aa624a5537ea
drielsz <adriellaurentino4@gmail.com>
2022-07-04 11:27:46 -0300

    :zap

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ for (let i = 0; i < ResultsJson.length; i++) {
   fetch(linkPokemon)
     .then((resp) => resp.json())
     .then(function (data) {
+      
+      let ids = data.id;
+
       let authors = data.sprites.front_default;
       let li = createNode("li");
 
@@ -33,16 +36,20 @@ for (let i = 0; i < ResultsJson.length; i++) {
       img.style.cssText = "width: 100%;height: 100%;background-size: auto;";
 
       let span = createNode("h1")
+      let h2 = createNode("h2")
+      h2.style.cssText = "color: white"
+
       span.style.cssText = ""
       img.src = authors;
 
       span.innerHTML = `${ResultsJson[i].name}`;
-
+      h2.innerHTML = `N°${ids}`
       append(li, img);
       append(li, span);
+      append(li, h2)
       append(ul, li);
     })
     .catch(function (error) {
-      console.log(error);
+
     });
 }
