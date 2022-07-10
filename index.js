ea4f4e9cefb9e7f53d3100e587a2d424124d539d
drielsz <adriellaurentino4@gmail.com>
2022-07-10 05:32:22 -0300

    :zap:

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 function Get(yourUrl) {
   var Httpreq = new XMLHttpRequest();
   Httpreq.open("GET", yourUrl, false);
@@ -13,47 +15,50 @@ function append(parent, el) {
   return parent.appendChild(el);
 }
 
-const ul = document.getElementById("authors");
+const ul = document.getElementById("authors"),
+      searchInput = document.getElementById("user-search"),
+      url = Get("https://pokeapi.co/api/v2/pokemon?limit=24&offset=0"),
+      JSONurl = JSON.parse(url),
+      ArrayJson = Array(JSONurl),
+      ResultsJson = ArrayJson[0].results;
 
-var url = Get("https://pokeapi.co/api/v2/pokemon?limit=120&offset=0");
-var JSONurl = JSON.parse(url);
-var ArrayJson = Array(JSONurl);
-var ResultsJson = ArrayJson[0].results;
+var pokeName,
+    pokemon, 
+    card;
 
-console.log(ResultsJson)
+pokeName = searchInput.value;
+console.log(pokeName)
 
 for (let i = 0; i < ResultsJson.length; i++) {
   var linkPokemon = ResultsJson[i].url;
   fetch(linkPokemon)
     .then((resp) => resp.json())
-    .then(function (data) {
-      console.log(url)   
+    .then(function (data) {  
 
-
-      let ids = data.id;
-
-      let authors = data.sprites.other.dream_world.front_default;
-      let li = createNode("li");
-
-      let img = createNode("img");
+      let ids = data.id,
+          authors = data.sprites.other.dream_world.front_default,
+          li = createNode("li"),
+          img = createNode("img"),
+          span = createNode("h1"),
+          h2 = createNode("h2");
 
       img.style.cssText = "width: 55%;height: 55%;background-size: auto;";
+      img.src = authors;
 
-      let span = createNode("h1")
-      let h2 = createNode("h2")
+
       h2.style.cssText = "color: white"
 
       span.style.cssText = ""
-      img.src = authors;
-
       span.innerHTML = `${ResultsJson[i].name}`;
       h2.innerHTML = `N°${ids}`
+      
+
       append(li, img);
       append(li, span);
       append(li, h2)
       append(ul, li);
     })
     .catch(function (error) {
-
+      console.log(error)
     });
 }
